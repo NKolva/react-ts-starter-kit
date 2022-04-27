99e9714eab90c66156cd9883738999986aea62ba
Nikita.Kolva <kolva.nikita@gmail.com>
2022-04-28 00:04:20 +0300

    Configuring Project Packages

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# !WORK IN PROGRESS!
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/app/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from 'src/assets/app/logo.svg';
+import classes from './App.module.scss';
+
+export const App: React.FC = () => {
+  return (
+    <div className={classes.App}>
+      <header className={classes.App_header}>
+        <img src={logo} className={classes.App_logo} alt="logo" />
+        <p>
+          Editt <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className={classes.App_link}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
